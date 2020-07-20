334c196aa283549df4480af1054f1e8904754bd3
wsor <wsor@gmail.com>
2020-07-20 18:36:04 -0400

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 minetest.register_on_joinplayer(function(player)
     player:get_inventory():set_size("main", 8*4)
     player:get_inventory():set_size("craftpreview", 1)
+    player:get_inventory():set_size("craftpreview", 1)
 end)
