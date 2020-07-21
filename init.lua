fb5231647d0f5417c91c3cf1f73842228207e6de
LoneWolfHT <lonewolf04361@gmail.com>
2020-07-20 18:43:29 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 minetest.register_on_joinplayer(function(player)
     player:get_inventory():set_size("main", 8*4)
     player:get_inventory():set_size("craftpreview", 1)
-    player:get_inventory():set_size("craftpreview", 1)
+    player:get_inventory():set_size("craftresult", 1)
+    player:get_inventory():set_size("craft", 3*3)
 end)
